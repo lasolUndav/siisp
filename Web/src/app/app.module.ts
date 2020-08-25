e69b819e1fb9f8ec0d5eede04a9c4cc0b9915ba4
federico <fede@intrigma.com>
2020-08-25 06:39:10 -0300

    Se agrega la al modulo de formulario de investigadores la vista

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioInvestigadorComponent } from './formulario-investigador/formulario-investigador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioInvestigadorComponent
   ],
   imports: [
     BrowserModule,
---
+++ Web/src/app/formulario-investigador/formulario-investigador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-investigador',
+  templateUrl: './formulario-investigador.component.html',
+  styleUrls: ['./formulario-investigador.component.css']
+})
+export class FormularioInvestigadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
